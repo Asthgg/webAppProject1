51b0823dad8eee923dc6785044458cb9c94f22ec
Andrea Gonzalez <asthg@outlook.com>
2019-08-18 17:17:28 -0500

    db

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
 
-const URI = 'http://ec2-52-55-96-2.compute-1.amazonaws.com/commentsdb'
+//const URI = 'http://ec2-52-55-96-2.compute-1.amazonaws.com'
 //const URI = 'mongodb://localhost/commentsdb'
+
+const URI = 'mongodb://db:27017'
+
 
 mongoose.connect(URI, {
 	useCreateIndex: true,
